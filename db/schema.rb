8a86ccd81280cbdfe98cd48b0bf128c500086e92
Joao Quaresma <joao.quaresma1991@gmail.com>
2018-02-07 21:49:36 +0000

    add associations between user and articles and add debugger for dev env in application.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207165224) do
+ActiveRecord::Schema.define(version: 20180207205509) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180207165224) do
     t.text     "post"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
