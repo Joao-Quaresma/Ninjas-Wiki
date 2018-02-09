430897aa26f49b647f28e6ddc7ff1fd364d4462d
Joao Quaresma <joao.quaresma1991@gmail.com>
2018-02-09 08:20:14 +0000

    add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Zappi Ninjas #{@user.username}"
       redirect_to root_path
     else
