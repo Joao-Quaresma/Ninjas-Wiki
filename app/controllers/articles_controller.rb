66da27f4154ac7426a4def395f6140f1739d13a7
Joao Quaresma <joao.quaresma1991@gmail.com>
2018-02-09 10:38:38 +0000

    add admin functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
     end
     
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can only delete your Posts"
         redirect_to root_path
       end
